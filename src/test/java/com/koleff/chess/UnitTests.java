886ad55415e4a378bd74c2e682fd4b41cd564400
sape <you@example.com>
2024-03-01 21:05:24 +0100

    R - optimized imports

--- src/test/java/com/koleff/chess/UnitTests.java
+++ src/test/java/com/koleff/chess/UnitTests.java
@@ -3,7 +3,6 @@ package com.koleff.chess;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Still not implemented! (Coming soon...)
